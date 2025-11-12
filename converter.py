f30b5cd2ffb8f85f0aaea6b1384bee2c86bd76a2
Ігор <guraligor301@gmail.com>
2025-11-12 22:34:09 +0200

    Added Fahrenheit to Kelvin conversion

--- converter.py
+++ converter.py
@@ -15,6 +15,16 @@ def kelvin_to_celsius(kelvin):
     return kelvin - 273.15
 
 
+
+def fahrenheit_to_kelvin(fahrenheit):
+ """Конвертує Фаренгейт у Кельвін"""
+ celsius = fahrenheit_to_celsius(fahrenheit)
+ return celsius_to_kelvin(celsius)
+
+
+
+
+
 print("=== Конвертер температур ===")
 print("1. Цельсій -> Фаренгейт")
 print("2. Фаренгейт -> Цельсій")
